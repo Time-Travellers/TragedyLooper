1177450df5feccce27f8c5929b101c3afe08def3
Pablo M <2apablomartin@gmail.com>
2017-05-07 18:04:03 +0200

    Archivo .java con la GUI de iniciar seison

--- src/tragedylooper/view/IniciarSesionUI.java
+++ src/tragedylooper/view/IniciarSesionUI.java
@@ -1,85 +1,79 @@
 package tragedylooper.view;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
-import javax.swing.border.Border;
 
-public class IniciarSesionUI extends JPanel {
+public class IniciarSesionUI extends JFrame {
 	
-	private JPanel user;
-	private JPanel password;
-	
+	private JTextField userIn;
+	private JPasswordField passwIn;
+	private JButton iniciarSesion;
+	private JButton crearCuenta;
 
 	public IniciarSesionUI() {
 		inicializarGUI();
 	}
-	
-	public void inicializarGUI() {
+
+	private void inicializarGUI() {
+		this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+		this.setSize(new Dimension(800, 600));
 		
-		this.setMaximumSize(new Dimension(500, 500));
+		JPanel cuadroEntrada = new JPanel();
+		cuadroEntrada.setLayout(new BoxLayout(cuadroEntrada, BoxLayout.Y_AXIS));
 		
-		//Panel user:
+		JPanel usuario =  new JPanel();
+		usuario.setLayout(new FlowLayout(FlowLayout.CENTER));
+		userIn = new JTextField(15);
+		usuario.add(new JLabel("Usuario"));
+		usuario.add(userIn);
+		usuario.setPreferredSize(new Dimension(300, 50));
+		usuario.setMaximumSize(usuario.getPreferredSize());
 		
-		this.user = new JPanel();
-		JTextField userIn = new JTextField(15);
-		userIn.setVisible(true);
-		JLabel userTitle = new JLabel("Usuario");
-		userIn.add(userTitle);
-		userTitle.setVisible(true);
-		this.user.add(userIn);
-		this.setVisible(true);
-		this.add(this.user);
+		JPanel pw =  new JPanel();
+		pw.setLayout(new FlowLayout(FlowLayout.CENTER));
+		passwIn = new JPasswordField(15);
+		passwIn.setEchoChar('*');
+		pw.add(new JLabel("Contraseña"));
+		pw.add(passwIn);
+		pw.setPreferredSize(new Dimension(300, 50));
+		pw.setMaximumSize(usuario.getPreferredSize());
 		
-		this.password=new JPanel();
+		iniciarSesion = new JButton("Iniciar Sesión");
+		crearCuenta = new JButton("Darse de Alta");
+
+		cuadroEntrada.add(Box.createRigidArea(new Dimension(1,20)));
+		cuadroEntrada.add(usuario);
+		cuadroEntrada.add(pw);
+		cuadroEntrada.add(Box.createRigidArea(new Dimension(1,20)));
+		cuadroEntrada.add(iniciarSesion);
+		cuadroEntrada.add(Box.createRigidArea(new Dimension(1,10)));
+		cuadroEntrada.add(crearCuenta);
+		cuadroEntrada.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, Color.black));
+		cuadroEntrada.setPreferredSize(new Dimension(205,220));
+		cuadroEntrada.setMaximumSize(cuadroEntrada.getPreferredSize());
 		
-		/*this.setMaximumSize(new Dimension(200, 100));
-		//this.setPreferredSize(new Dimension(500,500));
-		Border borde = BorderFactory.createLineBorder(Color.blue, 5);
-		JPanel ctrlPabel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		this.add(ctrlPabel);
-		this.setBackground(Color.red);
-		this.setVisible(true);
-		JLabel ctrlLabel = new JLabel("Usuario");
-		this.usuario=new JTextField(15);
-		ctrlLabel.add(this.usuario);
-		this.usuario.setBorder(borde);
 		
 		
-		this.add(ctrlLabel);
-		JLabel ctrlLabel2 = new JLabel("Contrase�a");
-		this.contrasena=new JTextField(15);
-		ctrlLabel2.add(this.contrasena);
-		this.contrasena.setBorder(borde);
-		this.add(ctrlLabel2);
-		this.setLayout(new GridLayout(2,0));
-		this.add(this.usuario);
-		this.add(this.contrasena);
-		
-		this.setMaximumSize(new Dimension(500, 500));
-		*/
-		
+		this.add(Box.createRigidArea(new Dimension(185, 150)));
+		this.add(cuadroEntrada);
+		this.setVisible(true);	
 	}
-	
+
 	
 	public static void main(String[] args) {
-		JFrame ventana=new JFrame("test");
-		ventana.getContentPane().add(new IniciarSesionUI());
-		ventana.setVisible(true);
-		ventana.setSize(800,600);
-		ventana.setBackground(Color.blue);
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//SwingUtilities.invokeLater(() -> new IniciarSesionUI());
+		new IniciarSesionUI();
+
 	}
 	
 	
