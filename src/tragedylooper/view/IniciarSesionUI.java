9c58407deb7c8d67bcdf7656d04743f95e2a92c1
Pablo M <2apablomartin@gmail.com>
2017-05-12 13:45:27 +0200

    Mejoras al panel de inicio de sesión

--- src/tragedylooper/view/IniciarSesionUI.java
+++ src/tragedylooper/view/IniciarSesionUI.java
@@ -3,34 +3,44 @@ package tragedylooper.view;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class IniciarSesionUI extends JFrame {
+import tragedylooper.mvc.IniSesionEvent;
+import tragedylooper.mvc.IniSesionEvent.IniSesionType;
+import tragedylooper.mvc.IniSesionListenable;
+import tragedylooper.mvc.IniSesionListener;
+
+public class IniciarSesionUI extends JPanel implements IniSesionListenable{
 	
 	private JTextField userIn;
-	private JPasswordField passwIn;
+	private JTextField passwIn;
 	private JButton iniciarSesion;
 	private JButton crearCuenta;
+	private ArrayList<IniSesionListener> listeners;
 
 	public IniciarSesionUI() {
+		listeners = new ArrayList<IniSesionListener>();
 		inicializarGUI();
 	}
 
 	private void inicializarGUI() {
-		this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		this.setSize(new Dimension(800, 600));
 		
-		JPanel cuadroEntrada = new JPanel();
-		cuadroEntrada.setLayout(new BoxLayout(cuadroEntrada, BoxLayout.Y_AXIS));
+
 		
 		JPanel usuario =  new JPanel();
 		usuario.setLayout(new FlowLayout(FlowLayout.CENTER));
@@ -42,16 +52,17 @@ public class IniciarSesionUI extends JFrame {
 		
 		JPanel pw =  new JPanel();
 		pw.setLayout(new FlowLayout(FlowLayout.CENTER));
-		passwIn = new JPasswordField(15);
-		passwIn.setEchoChar('*');
-		pw.add(new JLabel("ContraseÃ±a"));
+		passwIn = new JTextField(15);
+		pw.add(new JLabel("Contraseña"));
 		pw.add(passwIn);
 		pw.setPreferredSize(new Dimension(300, 50));
 		pw.setMaximumSize(usuario.getPreferredSize());
 		
-		iniciarSesion = new JButton("Iniciar SesiÃ³n");
+		iniciarSesion = new JButton("Iniciar Sesión");
 		crearCuenta = new JButton("Darse de Alta");
-
+		
+		JPanel cuadroEntrada = new JPanel();
+		cuadroEntrada.setLayout(new BoxLayout(cuadroEntrada, BoxLayout.Y_AXIS));
 		cuadroEntrada.add(Box.createRigidArea(new Dimension(1,20)));
 		cuadroEntrada.add(usuario);
 		cuadroEntrada.add(pw);
@@ -63,18 +74,75 @@ public class IniciarSesionUI extends JFrame {
 		cuadroEntrada.setPreferredSize(new Dimension(205,220));
 		cuadroEntrada.setMaximumSize(cuadroEntrada.getPreferredSize());
 		
+		JPanel cuadroEntradaConTitulo = new JPanel();
+		cuadroEntradaConTitulo.setLayout(new BoxLayout(cuadroEntradaConTitulo, BoxLayout.Y_AXIS) );
+		JLabel titulo =  new JLabel("Tragedy Looper");
+		Font font = new Font("personalizada", Font.BOLD, 26);
+		titulo.setFont(font);
+		cuadroEntradaConTitulo.add(titulo);
+		cuadroEntradaConTitulo.add(Box.createRigidArea(new Dimension(0, 50)));
+		cuadroEntradaConTitulo.add(cuadroEntrada);
+		
+		ImageIcon icon = new ImageIcon("src\\resources\\mainart.png");
+		JLabel imagen = new JLabel();
+		imagen.setIcon(icon);
+		
+		JPanel cuadroFinal = new JPanel();
+		cuadroFinal.setLayout(new BoxLayout(cuadroFinal, BoxLayout.X_AXIS));
+		cuadroFinal.add(Box.createRigidArea(new Dimension(125, 0)));
+		cuadroFinal.add(cuadroEntradaConTitulo);
+		cuadroFinal.add(Box.createRigidArea(new Dimension(100, 0)));
+		cuadroFinal.add(imagen);	
+		this.add(cuadroFinal);
+		
+		iniciarSesion.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 0; i < listeners.size(); ++i)
+					listeners.get(i).notificarIniSesion(new IniSesionEvent(
+							IniSesionType.IniciarSesion, userIn.getText(), passwIn.getText()));
+			}
+		});
+		
+		crearCuenta.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 0; i < listeners.size(); ++i)
+					listeners.get(i).notificarIniSesion(new IniSesionEvent(IniSesionType.DarseAlta));
+			}
+		});
 		
 		
-		this.add(Box.createRigidArea(new Dimension(185, 150)));
-		this.add(cuadroEntrada);
-		this.setVisible(true);	
+	}
+	
+	@Override
+	public void addIniSesionListener(IniSesionListener list) {
+		listeners.add(list);
 	}
 
 	
 	public static void main(String[] args) {
-		new IniciarSesionUI();
-
+		JFrame frame = new JFrame();
+		IniciarSesionUI ui = new IniciarSesionUI();
+		
+		frame.add(ui);
+		frame.setSize(new Dimension(1024, 768));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		IniSesionListener test =  new IniSesionListener(){
+			@Override
+			public void notificarIniSesion(IniSesionEvent e) {
+				System.out.println(e.getIniSesionType());
+				System.out.println(e.getUsuario());
+				System.out.println(e.getContrasena());
+			}
+		};
+		ui.addIniSesionListener(test);
+		
 	}
+
+
 	
 	
 }
