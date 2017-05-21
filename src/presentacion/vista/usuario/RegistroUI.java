f56e68d23ebb5ef7dca783b5b0c8906d2451fdef
Alejandro97JS <alejandro97JS@gmail.com>
2017-05-21 14:38:25 +0200

    Nueva clase RegistroUI (de la vista)
    Nueva clase RegistroUI, con la interfaz gráfica y métodos; se usa cuando el usuario pulsa "Registrarse" en la pantalla de inicio.

---
+++ src/presentacion/vista/usuario/RegistroUI.java
@@ -0,0 +1,161 @@
+package presentacion.vista.usuario;
+
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+
+
+/**
+ * Pantalla que se abre cuando el usuario pulsa "Registrarse"
+ * al iniciar sesion.
+ *
+ */
+
+public class RegistroUI extends JPanel{
+
+	//Interfaz a implementar si se quieren insertar listeners:
+	public interface RegistroUIListener {
+		void registrarsePulsado();
+	}
+	
+	private JTextField username;
+	private JPasswordField password;
+	private JPasswordField password2;
+	private JTextField email;
+	private JTextField edad;
+	private JTextField pais;
+	private JButton registrarse;
+	private RegistroUIListener rListener;
+	
+	public RegistroUI(RegistroUIListener regListener) {
+		this.rListener = regListener;
+		initGUI();
+	}
+	
+	private void initGUI() {
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		JLabel titulo = new JLabel("Rellena los siguientes campos...");
+		titulo.setFont(new Font("", 25, 25));
+		this.add(titulo);
+		this.add(new JLabel("Introduce tu nombre de usuario:"));
+		this.username = new JTextField();
+		this.add(username);
+		this.add(new JLabel("Introduce tu password:"));
+		this.password = new JPasswordField();
+		this.add(password);
+		this.add(new JLabel("Introduce tu password de nuevo:"));
+		this.password2 = new JPasswordField();
+		this.password2.addMouseListener(new MouseAdapter() {
+			public void mouseExited(MouseEvent e) {
+				if(!passwordsIguales()) {
+					JOptionPane.showMessageDialog(new JFrame(),
+							"Los passwords no coinciden",
+							"Comprueba tu password...",
+							JOptionPane.WARNING_MESSAGE);
+				}
+			}
+		});
+		this.add(password2);
+		this.add(new JLabel("Introduce tu email:"));
+		this.email = new JTextField();
+		this.add(email);
+		this.add(new JLabel("Introduce tu edad:"));
+		this.edad = new JTextField();
+		this.add(edad);
+		this.add(new JLabel("Introduce tu pais:"));
+		this.pais = new JTextField();
+		this.add(pais);
+		this.registrarse = new JButton("Registrarse");
+		this.registrarse.setEnabled(false);
+		this.registrarse.addActionListener((e)-> this.rListener.registrarsePulsado());
+		this.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				if(!todoRelleno()) registrarse.setEnabled(false);
+				else registrarse.setEnabled(true);
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				if(!todoRelleno()) registrarse.setEnabled(false);
+				else registrarse.setEnabled(true);
+			}
+			
+		});
+		this.add(registrarse);
+		this.setVisible(true);
+	}
+	
+	//Metodos para obtener cada uno de los campos...
+	
+	public String getUsername() {
+		return username.getText();
+	}
+	
+	@SuppressWarnings("deprecation")
+	public String getPassword() {
+		return password.getText();
+	}
+	
+	@SuppressWarnings("deprecation")
+	public String getPassword2() {
+		return password2.getText();
+	}
+	
+	public String getEmail() {
+		return email.getText();
+	}
+	
+	public String getEdad() {
+		return edad.getText();
+	}
+	
+	public String getPais() {
+		return pais.getText();
+	}
+	
+	// Metodo para saber si ha repetido el password correctamente:
+	public boolean passwordsIguales() {
+		return getPassword().equals(getPassword2());
+	}
+	
+	//Metodo para saber si ha rellenado todos los campos:
+	public boolean todoRelleno() {
+		return   !getUsername().equals("") && !getPassword().equals("") &&
+				!getPassword2().equals("") && !getEmail().equals("") &&
+				!getEdad().equals("") && !getPais().equals("");
+	}
+	
+	// Main para probar esta pantalla:
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(()-> {
+			JFrame frame = new JFrame("Ventana de Registro");
+			frame.setSize(600,508);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			RegistroUI registro = new RegistroUI(new RegistroUIListener() {
+
+				@Override
+				public void registrarsePulsado() {
+					System.out.println("Pulsado Registrarse");
+				}
+				
+			});
+			frame.setContentPane(registro);
+			frame.setVisible(true);
+		});
+	}
+
+}
