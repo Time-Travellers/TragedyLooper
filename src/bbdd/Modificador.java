cdb7f187760f907fa5b71332880398ffad6bfe66
ming <mingxguo@ucm.es>
2017-05-31 13:02:39 +0200

    ficheros

--- src/bbdd/Modificador.java
+++ src/bbdd/Modificador.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import presentacion.modelo.juego.InfoGuion;
+import presentacion.modelo.usuario.Administrador;
+import presentacion.modelo.usuario.Paises;
+import presentacion.modelo.usuario.Usuario;
 
 public class Modificador {
 	
@@ -12,10 +15,10 @@ public class Modificador {
 	
 	public static void main(String ... args) throws IOException {
 		
-		//file1.createNewFile();
-		//HashMap<String, Usuario> usuarios = new HashMap<String, Usuario>();
-		//usuarios.put("admin1", new Administrador("admin1", "admin1", true, "Administrador 1", 100, Paises.China, "admin1@gmail.com"));
-	    //new IO<Usuario>("usuarios.ser").almacenar(usuarios);
+		file1.createNewFile();
+		HashMap<String, Usuario> usuarios = new HashMap<String, Usuario>();
+		usuarios.put("admin1", new Administrador("admin1", "admin1", true, "Administrador 1", 100, Paises.China, "admin1@gmail.com"));
+	    new IO<Usuario>("usuarios.ser").almacenar(usuarios);
 		
 		//HashMap<String, InfoGuion> guiones = new IO<InfoGuion>("guiones.ser").leer();
 		//guiones.remove("Festival del algodon");
