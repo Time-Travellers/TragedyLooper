6fb05fbd658e2a02a87f28fcb8895c036f1b2e42
Errachete <rafaelherrer@hotmail.es>
2017-06-01 21:55:42 +0200

    Añadido modo normal con una sola ventana
    Para activar el modo de cuatro ventanas se debe dar args = "entrega" en
    el main

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -13,12 +13,29 @@ import presentacion.vista.usuario.iniciarsesion.IniciarSesionUI;
 
 public class Main {
 	
-	public static void main(String[] args) {
-		Log.setupLogging(Level.INFO);
+	public static void main(String ... args) {
+		Log.setupLogging(Level.SEVERE);
 		
-		// Inicializar 4 MVC con el mismo gestor
-		iniciarModoEntrega();
+		if(args.length > 0 && args[0] == "entrega")
+			// Inicializar 4 MVC con el mismo gestor para presentacion en clase
+			iniciarModoEntrega();
+		else
+			iniciarNormal();
 
+	}
+	
+	private static void iniciarNormal() {
+		FrameUI v = new FrameUI("Tragedy Looper", Toolkit.getDefaultToolkit().getScreenSize());
+		GUIController ctrl = new GUIController(v);
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				IniciarSesionUI ini = new IniciarSesionUI();
+				ini.addIniSesionListener(ctrl);
+				v.add(ini);
+				v.setVisible(true);
+			}
+		});
 	}
 
 	private static void iniciarModoEntrega() {
@@ -35,7 +52,6 @@ public class Main {
 		for(int i = 0; i < 4; ++i) {
 			FrameUI v = new FrameUI("Tragedy Looper " + (i + 1), vDim, posAncho[i], posAlto[i]);
 			GUIController ctrl = new GUIController(v);
-			v.setCtrl(ctrl);
 			SwingUtilities.invokeLater(new Runnable() {
 				@Override
 				public void run() {
--- src/presentacion/vista/FrameUI.java
+++ src/presentacion/vista/FrameUI.java
@@ -7,12 +7,31 @@ import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 
 import bbdd.Gestor;
-import presentacion.controlador.GUIController;
 
 public class FrameUI extends JFrame {
 	
 	private static final long serialVersionUID = 3437382482094237259L;
-	private GUIController ctrl;
+	
+	public FrameUI(String title, Dimension dimension) {
+		super(title);
+//		BufferedImage img = null;
+//		try {
+//			img = ImageIO.read(InicioAdminUI.class.getResource("/resources/fondo.png"));
+//		} catch (IOException e1) {
+//			Logger.getLogger("log").severe("Error al cargar la imagen de fondo de las pantallas");
+//		}
+//		this.setContentPane(new JLabel(new ImageIcon(img)));
+		//this.setLayout(new FlowLayout());
+		this.setSize(dimension.width, dimension.height);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.addWindowListener(new WindowAdapter(){
+			@Override
+			public void windowClosing(WindowEvent e) {
+				Gestor.getGestor().close();
+			}
+		});
+	}
 	
 	public FrameUI(String title, Dimension dim, int posX, int posY) {
 		super(title);
@@ -34,13 +53,5 @@ public class FrameUI extends JFrame {
 				Gestor.getGestor().close();
 			}
 		});
-	}
-	
-	public void setCtrl(GUIController ctrl) {
-		this.ctrl = ctrl;
-	}
-	
-	public GUIController getCtrl(){
-		return ctrl;
 	}
 }
